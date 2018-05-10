01563f71c1b27a779e733359f3b430d104e906f2
John Verrone <john_verrone@homedepot.com>
2018-05-10 15:30:49 -0400

    gitignore changes

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,8 +1,8 @@
 echo
 echo "Configuring jverrone3/dotfiles"
 
-touch ~/.gitignore
-echo ".cfg" >> .gitignore
+touch $HOME/.gitignore
+echo ".cfg" >> $HOME/.gitignore
 git clone --bare https://github.com/jverrone3/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
