0607227c554ff6082b2cc6a7177508c53ca0b971
John Verrone <john.verrone@gmail.com>
2018-07-10 18:23:52 -0400

    added patched fonts to script

--- scripts/iterm-setup.sh
+++ scripts/iterm-setup.sh
@@ -3,3 +3,10 @@ echo "Configuring iTerm..."
 
 cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 
+echo "Downloading patchable fonts..."
+
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
