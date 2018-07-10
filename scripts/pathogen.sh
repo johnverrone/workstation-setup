d5caaf94aedc76a9d5049ee26db0f0ef5d5849f0
John Verrone <john.verrone@gmail.com>
2018-07-10 18:13:34 -0400

    added iterm setup

--- scripts/pathogen.sh
+++ scripts/pathogen.sh
@@ -11,3 +11,4 @@ git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javas
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/tomasiser/vim-code-dark.git ~/.vim/bundle/vim-code-dark.git
--- setup.sh
+++ setup.sh
@@ -13,3 +13,4 @@ MY_DIR="$(dirname "$0")"
 source ${MY_DIR}/scripts/prezto-config.sh
 source ${MY_DIR}/scripts/pathogen.sh
 source ${MY_DIR}/scripts/dotfiles.sh
+source ${MY_DIR}/scripts/iterm-setup.sh
