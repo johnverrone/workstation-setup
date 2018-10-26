f40e1e4f2667ab5c9dc575e2cb6a1a80e9d48b90
John Verrone <john_verrone@homedepot.com>
2018-10-25 21:38:04 -0400

    adding prexto-contrib

--- scripts/prezto-config.sh
+++ scripts/prezto-config.sh
@@ -6,3 +6,9 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 echo "setting zsh as default shell"
 chsh -s /bin/zsh
 
+echo "configuring prezto-contrib"
+cd $ZPREZTODIR
+git clone https://github.com/belak/prezto-contrib contrib
+cd contrib
+git submodule init
+git submodule update
