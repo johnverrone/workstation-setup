d2f0295f1494ab0fd93bd060b1ce129a712f8a7e
John Verrone <johnverrone@fullstory.com>
2020-02-12 13:47:07 -0500

    added fonts

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -3,7 +3,5 @@ echo "Configuring iTerm..."
 cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 
 echo "Downloading patchable fonts..."
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd .. && rm -rf fonts
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
